a5912f4a76516c7283bf8c5528d8d82e87d7c4cc
John Vinh <john.michael.vinh2@gmail.com>
2025-02-21 19:22:44 -0500

    Add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 # Define output values for later reference
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "The name of the resource group to use for the resources"
 }
 
 output "vm_name" {
   value = azurerm_linux_virtual_machine.webserver.name
+  description = "The name of the virtual machine to provision"
 }
 
 output "nic_name" {
   value = azurerm_network_interface.webserver.name
+  description = "The name of the web server network interface"
 }
 
 output "public_ip" {
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
+  description = "The public IP address of the new Linux virtual machine"
 }
